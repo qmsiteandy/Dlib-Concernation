28f1e1afa03803e0fc29f0822783fc692209f7ca
qmsiteandy <ht93084@gmail.com>
2020-11-22 13:06:59 +0800

    commit

--- app.py
+++ app.py
@@ -118,5 +118,7 @@ def upload_file():
         return str(concern_degree)
     
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    app.run_server(
+        debug=True,
+    )
